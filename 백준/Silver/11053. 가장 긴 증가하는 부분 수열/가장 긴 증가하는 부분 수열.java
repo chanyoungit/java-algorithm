e380a90caa907fa93b88c5d029a3000377a16927
parkchanyoung <157675949+chanyoungit@users.noreply.github.com>
2024-11-04 10:25:19 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,58 +1,42 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    static int N;
-    static int[] A, dp;
+class Main {
+
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
         dp = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int LIS = 0;
+        int lis = 0;
         for (int i = 0; i < N; i++) {
-            int idx = BinarySearch(A[i], 0, LIS, LIS + 1);
+            int idx = search(arr[i], 0, lis, lis + 1);
 
             if (idx == -1) {
-                dp[LIS++] = A[i];
+                dp[lis++] = arr[i];
             } else {
-                dp[idx] = A[i];
+                dp[idx] = arr[i];
             }
         }
 
-        bw.write(LIS + "");
-
-        /*
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1;
-            for (int j = 0; j <= i; j++) {
-                if (A[i] > A[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-        }
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            if (max < dp[i]) max = dp[i];
-        }
-
-        bw.write(max + "");
-        */
-
-        bw.flush();
+        bw.write(lis + "");
         bw.close();
-        br.close();
     }
 
-    private static int BinarySearch(int num, int start, int end, int size) {
+    public static int search(int num, int start, int end, int size) {
         int res = 0;
+
         while (start <= end) {
             int mid = (start + end) / 2;
             if (num <= dp[mid]) {
@@ -62,6 +46,7 @@ public class Main {
                 start = mid + 1;
             }
         }
+
         if (start == size) {
             return -1;
         } else {
