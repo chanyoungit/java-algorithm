89ad74c330e0d370cf71868da373e2031e044504
parkchanyoung <157675949+chanyoungit@users.noreply.github.com>
2024-12-02 10:44:52 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 760 ms, Memory: 107704 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -6,30 +6,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        char[] chArr = br.readLine().toCharArray();
+        char[] arr = br.readLine().toCharArray();     // 주어진 문자열을 char 배열로 바로 변환
+
+        int[][] check = new int[arr.length][26];
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i] - 'a';
+
+            for (int j = 0; j < 26; j++) {      // 누적합
+                if (i != 0) check[i][j] += check[i - 1][j];
+                if (j == num) check[i][j] += 1;
+            }
+        }
 
         int q = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
-            int answer = 0;
-            char ch = st.nextToken().charAt(0);
+            int num = st.nextToken().charAt(0) - 'a';
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            for (int j = s; j <= e; j++) {
-                if (ch == chArr[j]) {
-                    answer++;
-                }
+            if (s == 0) {
+                bw.write(check[e][num] + "\n");
+            } else {
+                bw.write(check[e][num] - check[s - 1][num] + "\n");
             }
-
-            sb.append(answer + "\n");
         }
 
-        bw.write(sb.toString() + "");
         bw.close();
     }
 }
