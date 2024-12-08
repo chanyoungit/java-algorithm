895ba390e9923f98524d7529e8dc495cc87d0b2f
parkchanyoung <157675949+chanyoungit@users.noreply.github.com>
2024-12-08 17:21:27 +0900

    [Gold IV] Title: 팰린드롬?, Time: 808 ms, Memory: 251108 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,46 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
+        int[] numArr = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] arr = new int[N + 1][N + 1];
+        for (int j = 1; j <= N; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                } else if (Math.abs(i - j) == 1) {
+                    if (numArr[i] == numArr[j]) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = 0;
+                    }
+                } else if (Math.abs(i - j) == 2) {
+                    if (numArr[i] == numArr[j]) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = 0;
+                    }
+                } else {
+                    if (numArr[i] == numArr[j] && arr[i + 1][j - 1] == 1) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = 0;
+                    }
+                }
+            }
         }
 
         int M = Integer.parseInt(br.readLine());
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if (s == e) {
-                bw.write("1\n");
-            } else if (e - s == 1) {
-                if (arr[s] == arr[e]) bw.write("1\n");
-                else bw.write("0\n");
-            } else {
-                boolean check = true;
-                while (s < e) {
-                    if (arr[s] == arr[e]) {
-                        s++;
-                        e--;
-                    } else {
-                        check = false;
-                        bw.write("0\n");
-                        break;
-                    }
-                }
-
-                if (check) bw.write("1\n");
-            }
+            bw.write(arr[a][b] + "\n");
         }
 
         bw.close();
