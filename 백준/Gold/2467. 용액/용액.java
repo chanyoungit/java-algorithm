263b7d82c61c861b8826f70f95b89d55ca6fed3c
parkchanyoung <157675949+chanyoungit@users.noreply.github.com>
2025-01-16 23:55:55 +0900

    [Gold V] Title: 용액, Time: 360 ms, Memory: 28676 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static int min;
+    static int[] answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        answer = new int[2];
+        min = Integer.MAX_VALUE;
+
+        binaryCheck();
+
+        bw.write(answer[0] + " " + answer[1]);
+        bw.close();
+    }
+
+    public static void binaryCheck() {
+        int s = 0;
+        int e = N - 1;
+
+        while (e -s != 0) {
+            int sum = arr[s] + arr[e];
+
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                answer[0] = arr[s];
+                answer[1] = arr[e];
+            }
+
+            if (min == 0) {
+                return;
+            } else if (sum > 0) {
+                e--;
+            } else {
+                s++;
+            }
+        }
+    }
+}
