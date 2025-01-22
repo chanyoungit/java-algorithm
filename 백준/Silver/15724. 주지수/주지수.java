0958ac409bb0ff560aa92b0a127beace5c3239a0
Chanyoung Park <157675949+chanyoungit@users.noreply.github.com>
2025-01-22 11:49:17 +0900

    [Silver I] Title: 주지수, Time: 1116 ms, Memory: 124620 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+// map.put(str, map.getOrDefault(str, 0) + 1);
+// ArrayList<String> al = new ArrayList<>(map.keySet());
+// ArrayList<Integer> al = new ArrayList<>(map.values());
+// Collections.sort(al, new Comparator<String>() { ... return o1.compareTo(o2)};
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            dp[i][0] = 0;
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+//        for (int i = 1; i <= M; i++) {
+//            for (int j = 2; j <= N; j++) {
+//                dp[j][i] += dp[j - 1][i];
+//            }
+//        }
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
+
+            int answer = 0;
+
+            if (startY != 1) {
+                for (int j = startX; j <= endX; j++) {
+                    answer += dp[j][endY] - dp[j][startY - 1];
+                }
+            } else {
+                for (int j = startX; j <= endX; j++) {
+                    answer += dp[j][endY];
+                }
+            }
+
+            sb.append(answer + "\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
