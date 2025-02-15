0bbfffa337cad15a5c1b88ff8338f2d0a0d4d6e8
Chanyoung Park <157675949+chanyoungit@users.noreply.github.com>
2025-02-15 22:30:00 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 152 ms, Memory: 18644 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+// map.put(str, map.getOrDefault(str, 0) + 1);
+// ArrayList<String> al = new ArrayList<>(map.keySet());
+// ArrayList<Integer> al = new ArrayList<>(map.values());
+// Collections.sort(al, new Comparator<String>() { ... return o1.compareTo(o2)};
+
+// public int compare(Node o1, Node o2) {
+//    return Double.compare(o1.weight, o2.weight);
+// }
+
+// Arrays.sort(a, Collections.reverseOrder())
+
+// Lower Bound -> LIS
+//while (s < e) {
+//    int mid = (s + e) / 2;
+//    if (al.get(mid) >= num) {
+//        e = mid;
+//    } else {
+//        s = mid + 1;
+//    }
+//}
+// al.set(e, num);
+
+// Math.round(num1 * 100.0) / 100.0 -> 3.14
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N];
+        int[] arr = new int[N];
+        int[] prev = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+            }
+        }
+
+        int LIS = 0;
+        int index = 0;
+        for (int i = 0; i < N; i++) {
+            if (LIS < dp[i]) {
+                LIS = dp[i];
+                index = i;
+            }
+        }
+
+        bw.write(LIS + "\n");
+
+        Integer[] answer = new Integer[LIS];
+
+        for (int i = 0; i < LIS; i++) {
+            answer[i] = arr[index];
+            index = prev[index];
+        }
+
+
+        for (int i = LIS - 1; i >= 0; i--) {
+            bw.write(answer[i] + " ");
+        }
+
+        bw.close();
+    }
+}
