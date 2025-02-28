8be9f8c5e0d2279ac7ad7f95529e91deec885ee3
Chanyoung Park <157675949+chanyoungit@users.noreply.github.com>
2025-02-28 21:32:45 +0900

    [D3] Title: 부분 수열의 합, Time: 674 ms, Memory: 129,868 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			
+			int[] arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i=0; i<N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int answer = 0;
+			ArrayList<Integer> al = new ArrayList<>();
+			al.add(0);
+			
+			for (int i=0; i<N; i++) {
+				ArrayList<Integer> newAl = new ArrayList<>();
+				
+				for (int num : al) {
+					int sum = num + arr[i];
+					if (sum == K) {
+						answer++;
+					}
+					newAl.add(sum);
+				}
+				al.addAll(newAl);
+			}
+			
+			bw.write("#" + test_case + " " + answer + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
