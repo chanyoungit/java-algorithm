a1f163b988747e6a2e1a234a269be134270c7f0c
Chanyoung Park <157675949+chanyoungit@users.noreply.github.com>
2025-03-13 13:40:58 +0900

    [Silver III] Title: N번째 큰 수, Time: 804 ms, Memory: 276380 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+// map.put(str, map.getOrDefault(str, 0) + 1);
+// ArrayList<String> al = new ArrayList<>(map.keySet());
+// ArrayList<Integer> al = new ArrayList<>(map.values());
+// Collections.sort(al, new Comparator<String>() { ... return o1.compareTo(o2)};
+// String str = String.valueOf(ch).repeat(5);
+// int[] alpha = new int[26];
+
+//value = value.substring(0, value.length() - 1);
+//value = new StringBuilder(value).reverse().toString();
+
+// public int compare(Node o1, Node o2) {
+//    return Double.compare(o1.weight, o2.weight);
+// }
+
+// Arrays.sort(a, Collections.reverseOrder())
+
+// Lower Bound -> LIS
+//while (s < e) {
+//    int mid = (s + e) / 2;
+//    if (al.get(mid) >= num) {
+//        e = mid;
+//    } else {
+//        s = mid + 1;
+//    }
+//}
+// al.set(e, num);
+
+// Math.round(num1 * 100.0) / 100.0 -> 3.14
+// IFNULL(name, 'park') AS name, WHERE NAME IS NOT NULL, COUNT(DISTINCT NAME)
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            if (i == N - 1) {
+                answer = queue.poll();
+            }
+
+            queue.poll();
+        }
+
+        bw.write(answer + "");
+        bw.close();
+    }
+}
