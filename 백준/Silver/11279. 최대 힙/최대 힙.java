56192c587355735a7b47dd805fa43db75470584e
Chanyoung Park <157675949+chanyoungit@users.noreply.github.com>
2025-03-13 14:08:10 +0900

    [Silver II] Title: 최대 힙, Time: 316 ms, Memory: 26600 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+// map.put(str, map.getOrDefault(str, 0) + 1);
+// ArrayList<String> al = new ArrayList<>(map.keySet());
+// ArrayList<Integer> al = new ArrayList<>(map.values());
+// Collections.sort(al, new Comparator<String>() { ... return o1.compareTo(o2)};
+// String str = String.valueOf(ch).repeat(5);
+// int[] alpha = new int[26];
+
+//value = value.substring(0, value.length() - 1);
+//value = new StringBuilder(value).reverse().toString();
+
+// public int compare(Node o1, Node o2) {
+//    return Double.compare(o1.weight, o2.weight);
+// }
+
+// Arrays.sort(a, Collections.reverseOrder())
+
+// Lower Bound -> LIS
+//while (s < e) {
+//    int mid = (s + e) / 2;
+//    if (al.get(mid) >= num) {
+//        e = mid;
+//    } else {
+//        s = mid + 1;
+//    }
+//}
+// al.set(e, num);
+
+// Math.round(num1 * 100.0) / 100.0 -> 3.14
+// IFNULL(name, 'park') AS name, WHERE NAME IS NOT NULL, COUNT(DISTINCT NAME)
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(queue.poll() + "\n");
+                }
+            } else {
+                queue.add(num);
+            }
+        }
+
+        bw.close();
+    }
+}
